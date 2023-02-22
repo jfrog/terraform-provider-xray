c38db0090d98ad081ec52c220afccaed66548f07
Alex Hung <alexh@jfrog.com>
2023-02-22 14:28:36 -0800

    Add import tests for watch resources
    Add special resource ID handling when importing watch in scope of a project.
    
    Update doc for the same

--- pkg/xray/resource_xray_watch.go
+++ pkg/xray/resource_xray_watch.go
@@ -1,6 +1,11 @@
 package xray
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/jfrog/terraform-provider-shared/util"
 	"github.com/jfrog/terraform-provider-shared/validator"
@@ -15,7 +20,19 @@ func resourceXrayWatch() *schema.Resource {
 		Description:   "Provides an Xray watch resource.",
 
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: func(ctx context.Context, d *schema.ResourceData, meta any) ([]*schema.ResourceData, error) {
+				tflog.Debug(ctx, fmt.Sprintf("d.Id(): %s", d.Id()))
+				parts := strings.SplitN(d.Id(), ":", 2)
+				tflog.Debug(ctx, fmt.Sprintf("parts: %v", parts))
+
+				if len(parts) == 2 && parts[0] != "" && parts[1] != "" {
+					d.SetId(parts[0])
+					d.Set("project_key", parts[1])
+				}
+				tflog.Debug(ctx, fmt.Sprintf("d: %v", d))
+
+				return []*schema.ResourceData{d}, nil
+			},
 		},
 
 		CustomizeDiff: watchResourceDiff,
